139cf823a87800ae2d040dc8a8a36e8d91559a56
Programming Hero <programming.hero1@gmail.com>
2021-10-30 02:13:30 -0500

    handle 401

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import useAuth from './../../hooks/useAuth';
+import { useHistory } from 'react-router';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const { user } = useAuth();
+    const history = useHistory();
     useEffect(() => {
-        fetch(`http://localhost:5000/orders?email=${user.email}`)
-            .then(res => res.json())
+        fetch(`http://localhost:5000/orders?email=${user.email}`, {
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('idToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                else if (res.status === 401) {
+                    history.push('/login');
+                }
+
+            })
             .then(data => setOrders(data));
     }, [])
 
